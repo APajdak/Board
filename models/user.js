c5137269ec4ee92378db31e00ca25a3d4777c85d
APajdak <a.pajdak@gmail.com>
2019-03-10 21:21:22 +0100

    User validation

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -11,7 +12,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    minlength: 1,
+    minlength: 5,
     trim: true,
     unique: true
   },
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema({
     minlength: 3,
     maxlength: 300
   },
-  registered: {
+  registeredAt: {
     type: Date,
     default: Date.now
   },
@@ -33,6 +34,26 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+function validateUser(user) {
+  const schema = {
+    name: Joi.string()
+      .min(2)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(3)
+      .max(255)
+      .required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
 module.exports = {
-  User
+  User,
+  validateUser
 };
