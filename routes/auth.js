9849e172f1df59d1b8db02e487f3706ee32c6fff
APajdak <a.pajdak@gmail.com>
2019-04-13 16:31:56 +0200

    Login controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,8 @@
-const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
-const Joi = require("joi");
-const { User } = require("../models/user");
 
-//Login
-router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+const controller = require("../controllers/auth");
 
-  let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("Invalid email or password");
-
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send("Invalid email or password");
-
-  const token = user.createToken();
-
-  res.json({ user: { name: user.name, slug: user.slug }, token });
-});
-
-function validate(req) {
-  const schema = {
-    email: Joi.string()
-      .min(5)
-      .required()
-      .email(),
-    password: Joi.string()
-      .min(5)
-      .max(300)
-      .required()
-  };
-
-  return Joi.validate(req, schema);
-}
+router.post("/", controller.logIn);
 
 module.exports = router;
