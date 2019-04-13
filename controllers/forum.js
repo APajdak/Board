2d8fc48553ef90f52e699d8180e492659492cd44
APajdak <a.pajdak@gmail.com>
2019-04-13 14:53:33 +0200

    Forum controller

---
+++ controllers/forum.js
@@ -0,0 +1,42 @@
+const { Forum, forumValidation } = require("../models/forum");
+
+const getForumThreads = async (req, res) => {
+  const forum = await Forum.findOne({ slug: req.params.slug })
+    .select("_id")
+    .populate({
+      path: "threads",
+      select: "title author createdAt latestUpdate -_id",
+      options: {
+        sort: {
+          latestUpdate: -1
+        }
+      },
+      populate: {
+        path: "author",
+        select: "role name -_id"
+      },
+      populate: {
+        path: "latestUpdate.user",
+        select: "name slug -_id"
+      }
+    });
+  res.send(forum);
+};
+
+const addNewForum = async (req, res) => {
+  const { error } = forumValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const forum = new Forum({ name: req.body.name });
+  await forum.save();
+
+  res.status("200").send({
+    id: forum._id,
+    name: forum.name
+  });
+};
+
+module.exports = {
+  getForumThreads,
+  addNewForum
+};
