ec3ccd92d8072b4f8bcadec0fe9e32d064df2c72
APajdak <a.pajdak@gmail.com>
2019-05-16 17:34:13 +0200

    Added Header styles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container mt-4">
         <Header />
         {this.props.children}
       </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,26 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import Octicon, { Person, SignOut, SignIn } from "@githubprimer/octicons-react";
 
 class Header extends React.Component {
   renderLinks() {
     if (this.props.authenticated) {
       return (
-        <div>
-          <span>
-            Logged as:
-            <Link to={`/profile/${this.props.user.slug}`}>
+        <div className="float-right">
+          <span className="mr-4">
+            <Link
+              to={`/profile/${this.props.user.slug}`}
+              style={{ textDecoration: "none" }}
+            >
               {this.props.user.name}
+              <Octicon
+                icon={Person}
+                verticalAlign="middle"
+                size="medium"
+                className="ml-2"
+              />
             </Link>
           </span>
-          <Link to="/signout"> Sign out</Link>
+          <Link to="/signout" style={{ textDecoration: "none" }}>
+            Sign out
+            <Octicon
+              icon={SignOut}
+              verticalAlign="middle"
+              size="medium"
+              className="ml-2"
+            />
+          </Link>
         </div>
       );
     } else {
       return (
-        <div>
-          <Link to="/signin">Sign in</Link>
-          <Link to="/signup">Sign Up</Link>
+        <div className="float-right">
+          <span className="mr-4">
+            <Link to="/signin" style={{ textDecoration: "none" }}>
+              Sign in
+              <Octicon
+                icon={SignIn}
+                verticalAlign="middle"
+                size="medium"
+                className="ml-2"
+              />
+            </Link>
+          </span>
+          <Link to="/signup">
+            <span className="btn btn-primary" style={{ fontSize: "1.875rem" }}>
+              Sign up
+            </span>
+          </Link>
         </div>
       );
     }
@@ -28,10 +59,10 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div className="row">
-        <div className="col-sm-8">Discussion Board</div>
-        <div className="col-sm-4">{this.renderLinks()}</div>
-      </div>
+      <header className="row align-items-center" style={{ height: "100px" }}>
+        <h2 className="col-auto mr-auto">Discussion Board</h2>
+        <h2 className="col-auto">{this.renderLinks()}</h2>
+      </header>
     );
   }
 }
