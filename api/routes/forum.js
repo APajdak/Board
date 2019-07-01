a7685dee688584aad7a867fdb7274d6f24b992cf
APajdak <a.pajdak@gmail.com>
2019-07-01 18:04:19 +0200

    Forum update validation

--- api/routes/forum.js
+++ api/routes/forum.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const controller = require("../controllers/forum");
 const isLogged = require("../middlewares/isLogged");
+const isAdmin = require("../middlewares/isAdmin");
 
 router.get("/", controller.getForums);
 
 router.get("/:slug", controller.getForumThreads);
 
 router.post("/", isLogged, controller.addNewForum);
+
+router.patch("/:slug", isLogged, isAdmin, controller.addNewForum);
 
 module.exports = router;
