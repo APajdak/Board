da24b37bc9e1a968a65b19e722c4c2e40b05206b
APajdak <a.pajdak@gmail.com>
2019-07-17 21:03:05 +0200

    minor renaming files

--- client/src/components/post/PostItem.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class PostItem extends Component {
-  render() {
-    return <div>PostItem</div>;
-  }
-}
-
-export default PostItem;
---
+++ client/src/components/post/PostListItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PostListItem extends Component {
+  render() {
+    return <div>PostListItem</div>;
+  }
+}
+
+export default PostListItem;
